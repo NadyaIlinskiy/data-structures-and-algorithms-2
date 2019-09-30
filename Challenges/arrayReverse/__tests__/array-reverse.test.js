967bb1988608112770fc2f71d7cddcf40f4ebce8
Nadya Ilinskaya <nadya.ilinskiy@gmail.com>
2019-09-29 20:48:54 -0700

    updating eslint config to get rid of lint errors in ci, change double quotes to single quots

--- Challenges/arrayReverse/__tests__/array-reverse.test.js
+++ Challenges/arrayReverse/__tests__/array-reverse.test.js
@@ -1,10 +1,10 @@
-"use strict";
+'use strict';
 
 // Require the module we're testing
 const Rev = require("../array-reverse.js");
 
-describe("Reverse array via new array", () => {
-  it("returnes reversed array - numbers", () => {
+describe('Reverse array via new array', () => {
+  it('returnes reversed array - numbers', () => {
     let arr = [1, 2, 3, 4, 5, 6];
     let revArr = [6, 5, 4, 3, 2, 1];
     let ar = Rev.aReverseViaNewArr(arr);
@@ -12,7 +12,7 @@ describe("Reverse array via new array", () => {
     expect(ar).toEqual(revArr);
   });
 
-  it("returnes reversed array - negative numbers ", () => {
+  it('returnes reversed array - negative numbers', () => {
     let arr = [89, 2354, 3546, 23, 10, -923, 823, -12];
     let revArr = [-12, 823, -923, 10, 23, 3546, 2354, 89];
     let ar = Rev.aReverseViaNewArr(arr);
@@ -20,17 +20,17 @@ describe("Reverse array via new array", () => {
     expect(ar).toEqual(revArr);
   });
 
-  it("returnes reversed array - strings", () => {
+  it('returnes reversed array - strings', () => {
     let arr = ['one', 'two', 'three'];
-    let revArr = ["three", "two", "one"]
+    let revArr = ['three', 'two', 'one']
     let ar = Rev.aReverseViaNewArr(arr);
 
     expect(ar).toEqual(revArr);
   });
 });
 
-describe("Reverce array in one cycle", () => {
-  it("returnes reversed array - numbers", () => {
+describe('Reverce array in one cycle', () => {
+  it('returnes reversed array - numbers', () => {
     let arr = [1, 2, 3, 4, 5, 6];
     let revArr = [6, 5, 4, 3, 2, 1];
     let ar = Rev.aReverseInOneCycle(arr);
@@ -38,7 +38,7 @@ describe("Reverce array in one cycle", () => {
     expect(ar).toEqual(revArr);
   });
 
-  it("returnes reversed array - negative numbers ", () => {
+  it('returnes reversed array - negative numbers', () => {
     let arr = [89, 2354, 3546, 23, 10, -923, 823, -12];
     let revArr = [-12, 823, -923, 10, 23, 3546, 2354, 89];
     let ar = Rev.aReverseInOneCycle(arr);
@@ -46,14 +46,11 @@ describe("Reverce array in one cycle", () => {
     expect(ar).toEqual(revArr);
   });
 
-  it("returnes reversed array - strings", () => {
+  it('returnes reversed array - strings', () => {
     let arr = ['one', 'two', 'three'];
-    let revArr = ["three", "two", "one"]
+    let revArr = ['three', 'two', 'one']
     let ar = Rev.aReverseInOneCycle(arr);
 
     expect(ar).toEqual(revArr);
   });
-
-
-
 });
