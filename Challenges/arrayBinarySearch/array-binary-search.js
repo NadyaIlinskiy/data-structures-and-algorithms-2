b8a3355e56c71c97766fa6ed7b460497dacac0b5
Nadya Ilinskaya <nadya.ilinskiy@gmail.com>
2019-10-04 16:26:08 -0700

    adding jsdoc comment

--- Challenges/arrayBinarySearch/array-binary-search.js
+++ Challenges/arrayBinarySearch/array-binary-search.js
@@ -2,15 +2,17 @@
 
 module.exports = exports = {};
 
-/***
- * @param arr gets sorted array
- * @param num gets search key
- * @returns return the index of the array’s element that is equal to the search key, or -1 if the element does not exist
+
+/**
+  * @param  {} arr gets sorted array
+  * @param  {} searchKey gets search key
+  * @returns return the index of the array’s element that is equal to the search key, or -1 if the element does not exist
  */
+
 exports.aBinarySearch = (arr, searchKey) => {
   let index;
 
 
-  
+
   return index; 
 };
