dcb5c128f8a4ca000085ae9a7f9156a33bd5bd75
Nadya Ilinskaya <nadya.ilinskiy@gmail.com>
2019-10-05 17:48:31 -0700

    adding solution

--- Challenges/arrayBinarySearch/array-binary-search.js
+++ Challenges/arrayBinarySearch/array-binary-search.js
@@ -2,17 +2,37 @@
 
 module.exports = exports = {};
 
+const test1 = [4,8,15,16,23,9, 15];
+const input1 = 15;
+const test2 = [11,22,33,44,55,66,77];
+const input2 = 90;
 
-/**
-  * @param  {} arr gets sorted array
-  * @param  {} searchKey gets search key
-  * @returns return the index of the array’s element that is equal to the search key, or -1 if the element does not exist
- */
+function returnKey(arr,search) {
+  //check if middle index matches key
+  let firstIndex = 0;
+  let lastIndex = arr.length - 1;
+ 
+  while (firstIndex < lastIndex) {
+    let middle = Math.floor((firstIndex + lastIndex) / 2);
+    if (arr[middle] === search) {
+      return middle;
+    }
+    if (search < arr[middle]) {
+    // if search is less, then firstIndex stays same
+    // and lastIndex changes to middle minus 1
+      lastIndex = middle-1;
+      middle = Math.floor((firstIndex + lastIndex) / 2);
+    // divide that by two, and make that new middle
+    } else if (search > arr[middle]) {
+      firstIndex = middle+1;
+      middle = Math.floor((firstIndex + lastIndex) / 2);
+    }
+  }
+  return -1;
+}
 
-exports.aBinarySearch = (arr, searchKey) => {
-  let index;
 
-
-
-  return index; 
-};
+console.log(returnKey(test2, 22));
+console.log(returnKey(test2, 66));
+console.log(returnKey(test2, 9));
+console.log(returnKey(test1, 289));
