32c1c302a44d12dce47a9f48295c940bbba81e58
Nadya Ilinskaya <nilinskaya@seattlecca.org>
2020-01-20 17:42:38 -0800

    fixing linter errors

--- Data-Structures/hashtable/hashTable.js
+++ Data-Structures/hashtable/hashTable.js
@@ -2,43 +2,43 @@
 'use strict';
 
 class HashTable {
-    constructor() {
-        this.table = new Array(200);
-    }
+  constructor() {
+    this.table = new Array(200);
+  }
 
-    hash(string) {
-        const H = 37;
-        let total = 0;
+  hash(string) {
+    const H = 37;
+    let total = 0;
 
-        for (var i = 0; i < string.length; i++) {
-            total += H * total + string.charCodeAt(i);
-        }
-        total %= this.table.length;
-        if (total < 1) {
-            this.table.length - 1
-        }
-        return parseInt(total);
+    for (var i = 0; i < string.length; i++) {
+      total += H * total + string.charCodeAt(i);
     }
+    total %= this.table.length;
+    if (total < 1) {
+      this.table.length - 1;
+    }
+    return parseInt(total);
+  }
 
-    add(key, value) {
-        let ind = this.hash(key);
-        this.table[ind] = { key, value };
-    }
+  add(key, value) {
+    let ind = this.hash(key);
+    this.table[ind] = { key, value };
+  }
 
-    get(key) {
-        let ind = this.hash(key);
-        let value = this.table[ind];
-        return value;
-    }
+  get(key) {
+    let ind = this.hash(key);
+    let value = this.table[ind];
+    return value;
+  }
 
-    contains(key) {
-        let ind = this.hash(key);
-        if (this.table[ind]) {
-            return true;
-        } else {
-            return false;
-        }
+  contains(key) {
+    let ind = this.hash(key);
+    if (this.table[ind]) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
 }
 
--- Data-Structures/hashtable/tests/hashTable.test.js
+++ Data-Structures/hashtable/tests/hashTable.test.js
@@ -1,26 +1,26 @@
 'use strict';
 
 // Require the module we're testing
-const HashMap = require('../hashTable')
+const HashMap = require('../hashTable');
 
 describe('Hashtable function performs hash, add, get, contains functions', () => {
-    let hm = new HashMap();
+  let hm = new HashMap();
 
-    it('Adding a key/value to your hashtable results in the value being in the data structure ', () => {
-      let result = hm.add('bat', 'flying animal');
-      let expected = hm.contains('bat');
-        expect(expected).toBeTruthy();
-    });
+  it('Adding a key/value to your hashtable results in the value being in the data structure ', () => {
+    hm.add('bat', 'flying animal');
+    let expected = hm.contains('bat');
+    expect(expected).toBeTruthy();
+  });
 
-    it('retrieving based on a key returns the value stored', () => {
-        let result = hm.get('bat');
-        let expected = { key: 'bat', value: 'flying animal' };
-        expect(result).toEqual(expected);
-    });
-    it('Successfully returns null for a key that does not exist in the hashtable', () => {
-        let result = hm.get('cosmos'); 
-        expect(result).toBeFalsy();
-    });
+  it('retrieving based on a key returns the value stored', () => {
+    let result = hm.get('bat');
+    let expected = { key: 'bat', value: 'flying animal' };
+    expect(result).toEqual(expected);
+  });
+  it('Successfully returns null for a key that does not exist in the hashtable', () => {
+    let result = hm.get('cosmos'); 
+    expect(result).toBeFalsy();
+  });
 
 
 
