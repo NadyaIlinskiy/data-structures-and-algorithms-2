e0b78d064f75b4d29cb1735db678f2e2dddfe41a
Nadya Ilinskaya <nilinskaya@seattlecca.org>
2020-01-26 21:19:52 -0800

    linter errors

--- Challenges/repeatedWord/repeated-word.js
+++ Challenges/repeatedWord/repeated-word.js
@@ -2,43 +2,43 @@
 
 const repeatedWord = (string) => {
   
-    if ((typeof string) !== 'string')
-    {
-        return '';
-    }
-    let repeated = /(\b\w+\b)(?=.*\b\1\b)/gi;
-    let all = string.match(repeated);
-    if (!all){
-        return '';
-    }else {
+  if ((typeof string) !== 'string')
+  {
+    return '';
+  }
+  let repeated = /(\b\w+\b)(?=.*\b\1\b)/gi;
+  let all = string.match(repeated);
+  if (!all){
+    return '';
+  }else {
     return all[0];
-    }
-}
+  }
+};
 
 const repeatedWord2 = (string) => {
-    if ((typeof string) !== 'string') {
-        return '';
-    }
-    let words = string.split(" ");
+  if ((typeof string) !== 'string') {
+    return '';
+  }
+  let words = string.split(' ');
   
-    let wMap = {};
-    for (let i = 0; i < words.length; i++) {
-        let word = words[i]
-        if (!wMap[word]) {
-            wMap[word] = 1;
-        } else {
-            wMap[word] += 1;
-        }
+  let wMap = {};
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+    if (!wMap[word]) {
+      wMap[word] = 1;
+    } else {
+      wMap[word] += 1;
     }
+  }
 
-    for (let i = 0; i < words.length; i++) {
-        let word = words[i]
-        if (wMap[word] > 1) {
-            return  word; 
-        }
-    
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+    if (wMap[word] > 1) {
+      return  word; 
     }
-}
+    
+  }
+};
 
 
 module.exports = { repeatedWord, repeatedWord2};
